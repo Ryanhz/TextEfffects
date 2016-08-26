5053664c61614533fb578efe7d69dcc8a16d8c19
hzf <1810022686@qq.com>
2016-08-26 15:46:40 +0800

    ss

---
+++ TextEffects/TextEffects/TextAnimationLabel.swift
@@ -0,0 +1,21 @@
+//
+//  TextAnimationLabel.swift
+//  TextEffects
+//
+//  Created by hzf on 16/8/25.
+//  Copyright © 2016年 hzf. All rights reserved.
+//
+
+import UIKit
+
+class TextAnimationLabel: UILabel {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ TextEffects/TextEffects/TextLayerAniamtion.swift
@@ -0,0 +1,13 @@
+//
+//  TextLayerAniamtion.swift
+//  TextEffects
+//
+//  Created by hzf on 16/8/26.
+//  Copyright © 2016年 hzf. All rights reserved.
+//
+
+import UIKit
+
+class TextLayerAniamtion: NSObject {
+
+}
